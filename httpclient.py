b456e82210953704809246ccf7490589416ea7ea
JohnChen97 <chenjionghao@yahoo.com>
2021-09-28 00:44:00 -0600

    working on POST request

--- httpclient.py
+++ httpclient.py
@@ -24,6 +24,9 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+type_text = ['css', 'csv', 'html', 'javascript', 'plain', 'xml']
+type_application = ['x-www-form-urlencoded', 'octet-stream']
+
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,6 +36,8 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+        print(type(code))
+        print(type(self.code))
 
 
 class HTTPClient(object):
@@ -44,14 +49,20 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        re.findall('(?:HTTP\/\d\.\d\s)(\d{3})(?:.*)', data)
-        return None
+
+        code = re.findall('(?:HTTP\/\d\.\d\s)(\d{3})(?:.*)', data)
+        return int(code[0])
 
     def get_headers(self, data):
-        return None
+        #header = re.findall('\\r\\n\\r\\n', data)
+        header = data[0:data.find('\r\n\r\n')]
+        return header[0]
 
     def get_body(self, data):
-        return None
+        #body = re.findall('(?:\\r\\n\\r\\n)(.*)', data)
+        print(type(data.find('\r\n\r\n')))
+        body = data[data.find('\r\n\r\n'):-1]
+        return body
 
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -75,22 +86,52 @@ class HTTPClient(object):
         code = 500
         body = ""
         url_dict = self.interprate_url(url)
-        self.connect(url_dict['host'], int(url_dict['port']))
-        self.socket.sendall(b'GET / HTTP/1.1 \r\n')
-        self.socket.sendall(
-            ('Host: ' + url_dict['host'] + '\r\n').encode('utf-8'))
-        self.socket.sendall(b'Connection: close\r\n')
-        self.socket.sendall(b'\r\n')
         try:
-            received_data = self.recvall(self.socket)
-            code = received_data
+            self.connect(url_dict['host'], int(url_dict['port']))
 
-        return HTTPResponse(code, body)
+            self.socket.sendall(b'GET / HTTP/1.1 \r\n')
+            self.socket.sendall(
+                ('Host: ' + url_dict['host'] + '\r\n').encode('utf-8'))
+            self.socket.sendall(b'Connection: close\r\n')
+            self.socket.sendall(b'\r\n')
+            received_data = self.recvall(self.socket)
+            code = self.get_code(received_data)
+            body = self.get_body(received_data)
+        except ConnectionRefusedError:
+            #self.socket.send("HTTP/1.1 404 Not Found \r\n".encode('utf-8'))
+            http_response = HTTPResponse(code=404, body="")
+            return http_response
+        return HTTPResponse(code=code, body=body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
-        return HTTPResponse(code, body)
+        url_dict = self.interprate_url(url)
+        content_type = "application/x-www-form-urlencoded"
+        if self.interprate_file_type(args):
+            if self.interprate_file_type(args) in type_text:
+                content_type = "text/" + self.interprate_file_type(args)
+        try:
+            self.connect(url_dict['host'], int(url_dict['port']))
+            self.socket.sendall(
+                ('POST ' + args + ' / HTTP/1.1 \r\n').encode('utf-8'))
+            self.socket.sendall(
+                ('Host: ' + url_dict['host'] + '\r\n').encode('utf-8'))
+            self.socket.sendall(
+                ('Content-Type: ' + content_type + '\r\n').encode('utf-8'))
+            self.socket.sendall('Content-Length: ' +
+                                str(len(args) + '\r\n').encode('utf-8'))
+            self.socket.sendall(b'Connection: close\r\n')
+            self.socket.sendall(b'\r\n')
+            received_data = self.recvall(self.socket)
+            code = self.get_code(received_data)
+            body = self.get_body(received_data)
+            return HTTPResponse(code, body)
+        except ConnectionRefusedError:
+            #self.socket.send("HTTP/1.1 404 Not Found \r\n".encode('utf-8'))
+            http_response = HTTPResponse(code=404, body="")
+            return http_response
+        return HTTPResponse(code=code, body=body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
@@ -100,18 +141,33 @@ class HTTPClient(object):
 
     def interprate_url(self, url):
         http_dict = {}
-        if re.search('^http:\/\/.*$', url):
-            http_dict['scheme'] = 'http://'
-        if re.search('https?:\/\/[\w\.]+:\d+(\/[\w\.]+)?', url):
-            http_dict['host'] = re.findall(
-                '(?:https?:\/\/)([\w\.]+)(?::\d+(\/[\w\.]+)?)', url)[0][0]
-            http_dict['port'] = re.findall(
-                '(?:https?:\/\/[\w\.]+:)(\d+)(?:(\/[\w\.]+)?)', url)[0][0]
+        try:
+            if re.search('^http:\/\/.*$', url):
+                http_dict['scheme'] = 'http://'
+            if re.search('https?:\/\/[\w\.]+:\d+(\/[\w\.]+)?', url):
+                http_dict['host'] = re.findall(
+                    '(?:https?:\/\/)([\w\.]+)(?::\d+(\/[\w\.]+)?)', url)[0][0]
 
-        if re.search('(?:https?:\/\/[\w\.]+:\d+)(\/).*', url):
-            http_dict['path'] = re.findall(
-                '(?:https?:\/\/[\w\.]+:\d+\/)([\w\.]+)', url)[0][0]
-        return http_dict
+                http_dict['port'] = re.findall(
+                    '(?:https?:\/\/[\w\.]+:)(\d+)(?:(\/[\w\.]+)?)', url)[0][0]
+
+            elif re.search('https?:\/\/[\w\.]+(\/[\w\.]+)?', url):
+                http_dict['host'] = re.findall(
+                    '(?:https?:\/\/)([\w\.]+)(?:((\/[\w\.]+)?))', url)
+            if re.search('(?:https?:\/\/[\w\.]+:\d+)(\/).*', url):
+                http_dict['path'] = re.findall(
+                    '(?:https?:\/\/[\w\.]+:\d+\/)([\w\.]+)', url)[0][0]
+            return http_dict
+        except KeyError:
+            sys.stdout.write('something wrong with the dict\n')
+
+    def interprate_file_type(self, file_name):
+
+        if re.search('\.', file_name):
+            if re.search('(?:\w+\.)(\w{3,10})', file_name):
+                return re.findall('(?:\w+\.)(\w{3,10})', file_name)[0]
+        else:
+            return None
 
 
 if __name__ == "__main__":
@@ -123,12 +179,8 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         request_method = sys.argv[1]
         url = sys.argv[2]
-        print(client.interprate_url(url))
 
         http_response = client.command(sys.argv[2], sys.argv[1])
-        print(http_response.code)
-        print(http_response.body)
-        buffer = client.recvall(client.socket)
-        print(buffer)
+
     else:
         print(client.command(sys.argv[1]))
